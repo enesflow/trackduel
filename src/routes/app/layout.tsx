e3098b0dc67462f48263fdbfcefc0e957c7a3f63
enesflow <mail@enesin.xyz>
2025-06-18 21:55:44 +0300

    move to clean nextjs app

--- src/routes/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-import { RequestHandler, routeLoader$ } from "@builder.io/qwik-city";
-import {
-  useContext,
-  useContextProvider,
-  createContextId,
-} from "@builder.io/qwik";
-import { getAdminClient } from "~/lib/appwrite";
